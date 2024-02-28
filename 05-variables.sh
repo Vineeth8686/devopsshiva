474705a082f6d1e64248b914e57b20317960e662
chintu <vineethyadavc@gmail.com>
2024-02-28 13:08:02 +0530

    made chnages from windows

--- 05-variables.sh
+++ 05-variables.sh
@@ -3,4 +3,4 @@ echo "Hellow, Please enter user name:"
 read Username
 echo "hi $Username.Please enter your password"
 read -s Password:
-echo "The password is $Password".
+echo "The password is password".
