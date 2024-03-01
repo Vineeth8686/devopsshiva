79752eacbc9a33b751ea5cdabf326f38c20db34c
chintu <vineethyadavc@gmail.com>
2024-03-01 10:52:42 +0530

    made chnages from windows

---
+++ 07-ifCondition.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#check if the number is greater than 10 or not
+NUMBER=$1
+CONSTANT=10
+
+if [$1 >= $CONSTANT]
+then
+echo "$1 is the greater than 10"
+else
+echo "$1 is less than 10"
+if
